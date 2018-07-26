ff7dc4e23a1cb5d25b0a89bd0e88f8bc40b0542f
MrNickGreene <MrNickGreene@gmail.com>
2018-07-26 00:35:43 -0400

    Initial setup

---
+++ ObdCom/BluetoothService.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Acr.Collections;
+using Android.Accounts;
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Java.Util;
+
+namespace ObdCom
+{
+    public class BluetoothService : Service
+    {
+        private readonly BluetoothAdapter _bluetoothAdapter;
+        private BluetoothDevice _bluetoothDevice;
+        private BluetoothSocket _socket;
+        private UUID _uuid;
+        private string pairedMacAddress = "";
+
+        public BluetoothService()
+        {
+            _bluetoothAdapter = BluetoothAdapter.DefaultAdapter;
+
+            if (_bluetoothAdapter == null)
+            {
+                Log.Error("Error - bluetooth", "Bluetooth not supported - initial");
+            }
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            var bluetoothIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
+            base.StartService(bluetoothIntent);
+
+            return base.OnStartCommand(intent, flags, startId);
+        }
+
+        public override void OnCreate()
+        {
+            if (_bluetoothAdapter == null)
+            {
+                Log.Error("Error - bluetooth", "Bluetooth not supported - service start");
+                throw new NetworkErrorException("Bluetooth not supported");
+            }
+
+            if (!_bluetoothAdapter.IsEnabled)
+            {
+                Log.Error("Error - bluetooth", "Bluetooth disabled");
+                throw new NetworkErrorException("Bluetooth disabled");
+            }
+            
+            base.OnCreate();
+        }
+
+        public override bool OnUnbind(Intent intent)
+        {
+            return base.OnUnbind(intent);
+        }
+        public override void OnRebind(Intent intent)
+        {
+            base.OnRebind(intent);
+        }
+        public override void OnTaskRemoved(Intent rootIntent)
+        {
+            base.OnTaskRemoved(rootIntent);
+        }
+        
+        public string SendCommand(string message)
+        {
+            var response = "";
+            return response;
+        }
+        
+        public Dictionary<string, string> GetDevices()
+        {
+            var bondedDevices = new Dictionary<string, string>();
+            _bluetoothAdapter.BondedDevices.Each(x => bondedDevices.Add(x.Address, x.Name));
+            return bondedDevices;
+        }
+
+        public void SetDevice(string address)
+        {
+            if (address != pairedMacAddress)
+            {
+                _socket?.Close();
+
+                if (_bluetoothAdapter.BondedDevices.Any(x => x.Address == address))
+                {
+                    _uuid = new UUID(Int64.MaxValue, 0);
+                    pairedMacAddress = address;
+                    _bluetoothDevice = _bluetoothAdapter.BondedDevices.First(x => x.Address == pairedMacAddress);
+                    _socket = _bluetoothDevice.CreateRfcommSocketToServiceRecord(_uuid);
+                }
+                else
+                {
+                    _bluetoothDevice = null;
+                    pairedMacAddress = null;
+                }
+            }
+        }
+        
+        public void Read()
+        {
+            var buffer = new byte[2048];
+            _socket.InputStream.ReadAsync(buffer, 0, buffer.Length);
+        }
+
+        public void Send(byte[] message)
+        {
+            _socket.OutputStream.Write(message);
+        }
+
+        public async Task<string> SendAsync(byte[] message)
+        {
+            await _socket.OutputStream.WriteAsync(message, 0, message.Length);
+
+            return message.ToString();
+        }
+    }
+}
---
+++ ObdCom/MainActivity.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+using AlertDialog = Android.App.AlertDialog;
+
+namespace ObdCom
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    {
+        private BluetoothService _bluetoothService;
+        public string selectedBluetoothAddress;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            var drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            var toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            drawer.AddDrawerListener(toggle);
+            toggle.SyncState();
+
+            var navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+
+            _bluetoothService = new BluetoothService();
+        }
+
+        public override void OnBackPressed()
+        {
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            if(drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            var id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            var view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.nav_manage:
+                    break;
+                case Resource.Id.nav_bluetooth:
+                {
+                    StartService(new Intent(this, typeof(BluetoothService)));
+                    GetBluetoothSelectionAlert().Show();
+                    break;
+                }
+                case Resource.Id.nav_share:
+                    break;
+                case Resource.Id.nav_send:
+                    break;
+            }
+
+            var drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawer.CloseDrawer(GravityCompat.Start);
+            return true;
+        }
+
+        private AlertDialog.Builder GetBluetoothSelectionAlert()
+        {
+            var devices = _bluetoothService.GetDevices();
+
+            var selectedDeviceIndex = -1;
+            if (selectedBluetoothAddress != null && devices.Keys.Any(x => x == selectedBluetoothAddress))
+            {
+                selectedDeviceIndex = devices.Keys.ToList().IndexOf(devices.Keys.First(x => x == selectedBluetoothAddress));
+            }
+
+            var alertDialog = new AlertDialog.Builder(this);
+            alertDialog.SetSingleChoiceItems(devices.Select(x => x.Value).ToArray(), selectedDeviceIndex,
+                delegate(object sender, DialogClickEventArgs args)
+                {
+                    selectedDeviceIndex = args.Which;
+                });
+
+            alertDialog.SetTitle("Choose Bluetooth device");
+            alertDialog.SetNegativeButton("Cancel", (sender, args) => {});
+            alertDialog.SetPositiveButton("Save", (sender, args) =>
+            {
+                selectedBluetoothAddress = devices.ElementAt(selectedDeviceIndex).Key;
+                _bluetoothService.SetDevice(selectedBluetoothAddress);
+
+            });
+
+            return alertDialog;
+        }
+
+    }
+}
+
